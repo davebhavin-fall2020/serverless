7d430825527d1f600b1320064b550d8d591f35cb
davebhavin9 <dave.b@northeastern.edu>
2020-12-01 23:52:29 -0500

    initial comit

--- lambda.js
+++ lambda.js
@@ -1,11 +1,10 @@
 const aws = require("aws-sdk");
-const uuid1 = require('uuid');
+//const uuid1 = require('uuid');
 const domain_name = "prod.davebhavin.me"
 aws.config.update({ region: "us-east-1" });
 exports.emailService = function (event, context, callback) {
     let email= event.Records[0].Sns.Message;
     let ques = event.Records[0].Sns.QuestionId;
-	 let uuid= uuid1.v4();
 	 let emailMessage = 'https://'+domain_name+'/v1/question/'+ques;
 	 var emailParams = {
 		Destination: {
@@ -29,14 +28,7 @@ exports.emailService = function (event, context, callback) {
 		Source: "csye6225" + domain_name
 	  };
 
-      var sendPromise = new aws.SES({ apiVersion: '2010-12-01' }).sendEmail(emailParams).promise();
-				sendPromise.then(
-				  function (data) {
-					console.log("Email sent to: "+email+" Message id:: "+ data.MessageId);
-				  }).catch(
-					function (err) {
-					  console.error(err, err.stack);
-					});
+      
 
 	 
 };
